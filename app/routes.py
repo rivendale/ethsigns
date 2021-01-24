82b43fac1838276073e4a5d2b01574d512acca77
Nick <opencalcorg@gmail.com>
2021-01-23 23:06:03 -0500

    simple search and lookup feature added

--- app/routes.py
+++ app/routes.py
@@ -39,21 +39,32 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/',methods=['GET', 'POST'], defaults={"sym":"BLANK"})
-@app.route('/index',methods=['GET', 'POST'], defaults={"sym":"BLANK"})
-@app.route('/index/<sym>/', methods=['GET', 'POST'])
-def index(sym):
+@app.route('/',methods=['GET', 'POST'], defaults={"sym":"UNK"})
+@app.route('/index',methods=['GET', 'POST'], defaults={"sym":"UNK","typ":"UNK"})
+@app.route('/index/<typ>/<sym>/', methods=['GET', 'POST'])
+def index(typ,sym):
     form = SignsForm()
     if form.validate_on_submit():
         chkmonth = form.birthmonth.data
         chkday = form.birthday.data
         chkyear = form.birthyear.data
-        return redirect(url_for('index', sym=chkmonth))
+        typ = "NOT FOUND"
+        sym = "NOT FOUND"
+        checksign = Sign.query.filter_by(year=form.birthyear.data).first()
+        if checksign is not None:
+            if (checksign.month > chkmonth):
+                typ = checksign.dtype
+                sym = checksign.dsign
+            else:
+                if (checksign.day > chkday):
+                    typ = checksign.dtype
+                    sym = checksign.dsign
+                else:
+                    typ = checksign.btype
+                    sym = checksign.bsign
+        return redirect(url_for('index', typ=typ,sym=sym))
 
-    if sym == "BLANK":
-        sym = " "
-
-    return render_template('index.html', sym=sym,form=form,title='Home')
+    return render_template('index.html', sym=sym, typ=typ,form=form,title='Home')
 
 @app.route('/manage', methods=['GET', 'POST'])
 @login_required
@@ -61,10 +72,33 @@ def manage():
     form = ManageSigns()
 
     if form.validate_on_submit():
+        checkfirst = Sign.query.filter_by(year=form.startyear.data).first()
         addsign = Sign(year=form.startyear.data, month=form.startmonth.data,day=form.startday.data, bsign=form.beforesign.data , btype=form.beforetype.data,dsign=form.duringsign.data,dtype=form.duringtype.data)
-        db.session.add(addsign)
-        db.session.commit()
-        flash('Year added')
+        if checkfirst is None:
+            db.session.add(addsign)
+            db.session.commit()
+            flash('Year added')
+        else:
+            Sign.query.filter_by(year = form.startyear.data).delete()
+            db.session.commit()
+            db.session.add(addsign)
+            db.session.commit()
+            flash('Updated entry')
         return redirect(url_for('manage'))
     signs = Sign.query.order_by(Sign.year.asc()).all()
     return render_template('manage.html', signs=signs,form=form,title='Data Management')
+
+
+@app.route('/rmsym/<year>')
+@login_required
+def rmsym(year):
+    checkfirst = Sign.query.filter_by(year=year).first()
+    if checkfirst is None:
+        flash('Not found or other error')
+        return redirect(url_for('manage'))
+    else:
+        Sign.query.filter_by(year = year).delete()
+        db.session.commit()
+        flash('Deleted ')
+        db.session.commit()
+    return redirect(url_for('manage'))
