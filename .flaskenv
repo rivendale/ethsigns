7795d848fdb384e950e792795e6f1f68969aa72d
Hesbon <32098817+Hesbon5600@users.noreply.github.com>
2021-02-24 19:02:15 +0300

    feat (zodiac-sign): update year zodiac sign (#25)
    * feat (month-sign): uodate month zodiac sign (#23)
    
    - Create an endpoint to update month zodiac sign
    - write tests
    
    [Resolved #22]
    
    * feat (zodiac-sign): update year zodiac sign
    
    - create an endpoint to update year zodiac sign
    - write tests
    
    [Delivers #24]

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,6 @@
 FLASK_APP=ethsigns.py
 SECRET_KEY=kjdfskgldGDFLKJ34kSDOJFKSOIFJDSF3432jkrk3
+DATABASE_URI="sqlite:////<Path-to-db>/ethsigns.db"
+SQLALCHEMY_TRACK_MODIFICATIONS=false
+DEBUG=True
+FLASK_ENV="development"
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import (check_password_hash,
+                               generate_password_hash)
 
 from app import db, login
 
